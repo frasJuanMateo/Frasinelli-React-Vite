4446cbfb78d6a1051fc1f39a9f4814475cad5693
Juan Mateo Frasinelli <d46710280@alumnos.itr.edu.ar>
2024-08-14 20:44:54 -0300

    Se hizo el ejercicio 3

---
+++ src/Ejercicio3.jsx
@@ -0,0 +1,65 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import loadingIcon from "./assets/loading-image.gif"
+
+function Ejercicio3() {
+    const [loadingFlag, setLoadingFlag] = useState(true);
+    const [products, setProducts] = useState([]);
+    const [filteredList, setFilteredList] = useState([]);
+    const [search, setSearch] = useState("");
+    const [categories, setCategories] = useState([  ]);
+    const onChange = (event) => {
+      if (event.target.name == "search") {setSearch(event.target.value);}
+      if (event.target.name == "category") {
+        if(event.target.value == "all") {setFilteredList(products);}
+        else {setFilteredList(products.filter(product => product.category == event.target.value));}
+      }
+    } 
+    useEffect(() => {
+        fetch("https://fakestoreapi.com/products")
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+          }
+          else {
+            return response.json()
+          }
+        })
+        .then((json) => {
+          setProducts(json);
+          setFilteredList(json);
+          setLoadingFlag(false)
+        })
+        .catch(error => {
+          console.error('Error fetching data:', error);
+          })
+    }, []);
+
+    useEffect(() => {
+      let array = ["all", ...products.map(product => product.category)];
+      array = array.filter((val, id, array) => {
+        return array.indexOf(val) == id;  
+     });
+     setCategories(array)
+  }, [products]);
+    
+
+  return (
+    <>
+      {loadingFlag && <img src={loadingIcon} style={{width: "90px", height: "90px"}}></img>}
+      {!loadingFlag &&
+      <>
+        <input type="text" name='search' onChange={onChange}/><br />
+        <select name="category" onChange={onChange}>
+          {categories.map((category, index) => <option key={index}>{category}</option>)}
+        </select>
+
+        <h3>Lista de Productos:</h3>
+        <ul>{filteredList.filter(product => search == "" ? true : product.title.toLowerCase().startsWith(search)).map(product => <li key={product.id}>{product.title} ${product.price}</li>)}</ul>
+      </>
+      }
+    </>
+  )
+}
+
+export default Ejercicio3
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Ejercicio1 from './Ejercicio1.jsx'
 import Ejercicio2 from './Ejercicio2.jsx'
+import Ejercicio3 from './Ejercicio3.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Ejercicio1 />
-    <Ejercicio2 />
+    {/*<Ejercicio1 />
+    <Ejercicio2 />*/}
+    <Ejercicio3 />
   </React.StrictMode>,
 )
